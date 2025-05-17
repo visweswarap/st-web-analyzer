fe873fc1d49c1d184936c9c8d220a4400fcb3fc9
Vish Pepala <vish@claz.org>
2025-05-17 13:13:04 +0530

    Flask app enhancements to show midcap & small cap separately

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import sqlite3
 import os
 
@@ -6,20 +6,33 @@ app = Flask(__name__)
 
 DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'mutualfunds.db')
 
-def get_db_data():
+def get_db_data(category=None):
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-    cur.execute('SELECT * FROM stocks_by_fund')
+    if category:
+        cur.execute('SELECT * FROM stocks_by_fund WHERE category=?', (category,))
+    else:
+        cur.execute('SELECT * FROM stocks_by_fund')
     rows = cur.fetchall()
     conn.close()
     return rows
 
 @app.route('/')
-def index():
-    data = get_db_data()
+def home():
+    return render_template('home.html')
+
+@app.route('/small-cap')
+def small_cap():
+    data = get_db_data(category='small-cap')
     columns = data[0].keys() if data else []
-    return render_template('index.html', data=data, columns=columns)
+    return render_template('index.html', data=data, columns=columns, title='Small Cap')
+
+@app.route('/mid-cap')
+def mid_cap():
+    data = get_db_data(category='mid-cap')
+    columns = data[0].keys() if data else []
+    return render_template('index.html', data=data, columns=columns, title='Mid Cap')
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
